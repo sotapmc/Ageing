0e4c1d524236b0424598670d528a73266712ac07
Subilan <suyuanliang1995@gmail.com>
2020-07-25 10:49:41 +0800

    Rewrite onPlayerJoin doing stuff

--- src/main/java/org/sotap/Ageing/Events.java
+++ src/main/java/org/sotap/Ageing/Events.java
@@ -2,37 +2,32 @@ package org.sotap.Ageing;
 
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-
-import java.util.UUID;
 
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 
 public class Events implements Listener {
     public Ageing plug;
-    public Timer timer;
 
     public Events(Ageing plug) {
         this.plug = plug;
-        this.timer = new Timer(plug);
     }
 
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent e) {
         Player p = e.getPlayer();
-        if (!p.hasPlayedBefore()) {
-            this.plug.ageData.set(p.getName(), 0);
-            this.plug.saveData();
+        String uuid = p.getUniqueId().toString();
+        if (!p.hasPlayedBefore() || !this.plug.ageData.contains(uuid)) {
+            String name = p.getName();
+            this.plug.log(G.translateColor(G.info + "Detected a new player &a" + name + "&r, initializing data..."));
+            this.plug.ageData.set(uuid + ".username", name);
+            this.plug.ageData.set(uuid + ".age", 0);
+            this.plug.ageData.set(uuid + ".exp", 0);
+            this.plug.log(G.translateColor(G.success + "Initialization OK"));
+        } else {
+            if (this.plug.ageData.getString(uuid + ".username") != p.getName()) {
+                this.plug.ageData.set(uuid + ".username", p.getName());
+            }
         }
-        UUID uuid = p.getUniqueId();
-        this.timer.startTimerFor(uuid);
-    }
-
-    @EventHandler
-    public void onPlayerExit(PlayerQuitEvent e) {
-        Player p = e.getPlayer();
-        UUID uuid = p.getUniqueId();
-        this.timer.stopTimerFor(uuid);
     }
 }
