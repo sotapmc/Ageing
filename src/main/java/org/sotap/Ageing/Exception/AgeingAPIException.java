6287c103b629c58a3ba6dc42e051c2b439658337
Subilan <suyuanliang1995@gmail.com>
2020-07-28 16:10:26 +0800

    Exception for ageing api processing

---
+++ src/main/java/org/sotap/Ageing/Exception/AgeingAPIException.java
@@ -0,0 +1,8 @@
+package org.sotap.Ageing.Exception;
+
+@SuppressWarnings("serial")
+public class AgeingAPIException extends Exception {
+    public AgeingAPIException(String msg) {
+        super(msg);
+    }
+}
